de5d1f8ef4c56719e5f777c6f971a3d0c98e8f67
mba-open <mohamed.bahmed@open-groupe.com>
2019-07-30 10:37:20 +0200

    REST outis

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -9,4 +9,7 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
